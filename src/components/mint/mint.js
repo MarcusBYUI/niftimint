f102d5fddf16db64fde4af87130fcead7fa31ef0
MarcusBYUI <ojo18002@byui.edu>
2022-05-05 22:57:32 +0100

    fixed fee

--- src/components/mint/mint.js
+++ src/components/mint/mint.js
@@ -42,7 +42,7 @@ const Mint = () => {
       }
       const valueFee = {
         value: ethers.utils.parseEther(`${fee}`),
-        gasLimit: 30000,
+        gasLimit: 3000000,
       };
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
